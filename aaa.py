1f7f3ef78c63e8183d240853f8aafefb9dda7e78
biao-666 <54241188+biao-666@users.noreply.github.com>
2021-06-29 17:09:20 +0800

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,1 +1,16 @@
 print("hello word")
+import time
+import requests
+
+now_time = time.time()
+print("部署jenkins测试！！！")
+print(now_time)
+print('cqbteset')
+
+# 这是个测试
+url = 'http://www.weather.com.cn/data/sk/101010100.html'
+r = requests.get(url)
+print(f"返回数据编码{r.encoding}")
+r.encoding = 'utf-8'
+print(r.text)
+print(r.json())
